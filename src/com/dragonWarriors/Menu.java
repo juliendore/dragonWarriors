42f068c8d8b7e07e416131d918260cedd3a1b0bc
Julien DORE <julien.dore@le-campus-numerique.fr>
2020-05-13 16:47:24 +0200

    Iteration 2 ok

--- src/com/dragonWarriors/Menu.java
+++ src/com/dragonWarriors/Menu.java
@@ -1,5 +1,7 @@
 package com.dragonWarriors;
+
 import com.dragonWarriors.personnages.*;
+
 import java.util.Scanner;
 
 public class Menu {
@@ -16,6 +18,7 @@ public class Menu {
                 break;
             case "quit":
                 System.out.println("A bientôt !");
+                System.exit(0);
                 break;
             default:
                 System.out.println("Saisie incorrecte.");
@@ -37,6 +40,7 @@ public class Menu {
                 System.out.println("Que veux tu faire à présent ?");
                 System.out.println("infos");
                 System.out.println("rename");
+                System.out.println("play");
                 input = new Scanner(System.in);
                 userinput = input.nextLine();
                 switch (userinput) {
@@ -51,8 +55,12 @@ public class Menu {
                         warrior.setName(userinput);
                         warrior.sayHello();
                         break;
+                    case "play":
+                        int pos = 1;
+                        this.play(pos);
                     case "quit":
                         System.out.println("A bientôt !");
+                        System.exit(0);
                         break;
                     default:
                         System.out.println("Saisie incorrecte.");
@@ -65,6 +73,7 @@ public class Menu {
                 System.out.println("Que veux tu faire à présent ?");
                 System.out.println("infos");
                 System.out.println("rename");
+                System.out.println("play");
                 input = new Scanner(System.in);
                 userinput = input.nextLine();
                 switch (userinput) {
@@ -79,8 +88,12 @@ public class Menu {
                         magician.setName(userinput);
                         magician.sayHello();
                         break;
+                    case "play":
+                        int pos = 1;
+                        this.play(pos);
                     case "quit":
                         System.out.println("A bientôt !");
+                        System.exit(0);
                         break;
                     default:
                         System.out.println("Saisie incorrecte.");
@@ -89,11 +102,59 @@ public class Menu {
                 break;
             case "quit":
                 System.out.println("A bientôt !");
+                System.exit(0);
                 break;
             default:
                 System.out.println("Saisie incorrecte.");
                 this.select();
         }
+    }
+
+    public int play(int pos) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Position actuelle : " + pos + "/64");
+        System.out.println("'roll' pour lancer le dé.");
+        String userinput = input.nextLine();
+        switch (userinput) {
+            case "roll":
+                int dice1 = 0;
+                dice1 = 1 + (int) (Math.random() * 6);
+                pos += dice1;
+                System.out.println("Résultat du lancer de dé : " + dice1);
+                if (pos >= 64) {
+                    System.out.println("Bravo tu as fini que veux tu faire ?");
+                    System.out.println("restart");
+                    System.out.println("quit");
+                    input = new Scanner(System.in);
+                    userinput = input.nextLine();
+                    switch (userinput) {
+                        case "restart":
+                            pos = 0;
+                            this.play(pos);
+                            break;
+                        case "quit":
+                            System.out.println("A bientôt !");
+                            System.exit(0);
+                            break;
+                        default:
+                            System.out.println("Saisie incorrecte.");
+                            pos = 64;
+                    }
+
+                }
+                this.play(pos);
+                break;
+            case "quit":
+                System.out.println("A bientôt !");
+                System.exit(0);
+
+                break;
+            default:
+                System.out.println("Saisie incorrecte.");
+                this.play(pos);
+        }
+
+        return pos;
     }
 
     public Warrior newWarrior() {
